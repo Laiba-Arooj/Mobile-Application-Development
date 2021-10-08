22f8749e635517170dda006f6effdd021671b5ec
Laiba-Arooj <70106509+Laiba-Arooj@users.noreply.github.com>
2021-10-08 20:56:23 +0500

    Final Commit with 3 Layouts

---
+++ Duolingo/app/src/main/java/com/example/duolingo/LinearLayout.kt
@@ -0,0 +1,11 @@
+package com.example.duolingo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LinearLayout : AppCompatActivity() {
+    override fun onCreate(savedInstanceState : Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.linear_layout)
+    }
+}
