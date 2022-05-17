738dd44fa1477cd18a9769a42d2c6c31055b1af6
Laiba-Arooj <70106509+Laiba-Arooj@users.noreply.github.com>
2022-05-17 21:15:54 +0500

    Trial App for Tweet Link

---
+++ Check_App/app/src/main/java/com/example/check_app/MainActivity.kt
@@ -0,0 +1,63 @@
+package com.example.check_app
+
+import android.annotation.SuppressLint
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
+import com.android.volley.Request
+import com.android.volley.Response
+import com.android.volley.VolleyError
+import com.android.volley.toolbox.JsonObjectRequest
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
+import org.json.JSONException
+import org.json.JSONObject
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var btnPredict : Button
+    private lateinit var edtUrl : EditText
+    private lateinit var txtOutput : TextView
+    private lateinit var txtEmotion : TextView
+
+    private var url = "https://twtsentiment-fastapi.herokuapp.com/twt_link"
+    override fun onCreate(savedInstanceState : Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        btnPredict = findViewById(R.id.btn_predict)
+        edtUrl = findViewById(R.id.edt_url)
+        txtOutput = findViewById(R.id.txt_output)
+        txtEmotion = findViewById(R.id.txt_emotion)
+        btnPredict.setOnClickListener {
+            getSentiment()
+        }
+    }
+
+    @SuppressLint("SetTextI18n")
+    private fun getSentiment() {
+        val twtlink = edtUrl.text.toString().trim()
+        if (twtlink != "") {
+            val queue = Volley.newRequestQueue(this)
+            if (twtlink.isEmpty()) {
+                Toast.makeText(applicationContext, "Please add Country", Toast.LENGTH_LONG).show()
+                return
+            }
+
+            val stringRequest = StringRequest(Request.Method.GET,
+                "https://twtsentiment-fastapi.herokuapp.com/twt_link?link=$twtlink",
+                    Response.Listener<String> { response ->
+                        val obj = JSONObject(response)
+                        Log.d("Response", response.toString())
+                            txtOutput.text = obj.getString("sentence")
+                            txtEmotion.text = obj.getString("prediction")
+                    },
+                Response.ErrorListener { txtOutput.text = "That didn't work!" })
+
+            queue.add(stringRequest)
+
+        }
+    }
+}
